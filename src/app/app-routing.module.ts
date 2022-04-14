db95b1a7bf8fddb1825f3cc687bffeacb58cd3d0
Adrian Ravai <ravaiadrian@gmail.com>
2022-04-14 16:43:32 +0200

    Fix error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {UserComponent} from "./user/user.component";
 import {ProductComponent} from "./product/product.component";
 import {AuthGuard} from "./guards/auth.guard";
+import {ErrorComponent} from "./error/error.component";
 
 const routes: Routes = [
   { path: '', component: UserComponent },
-  { path: 'products', component: ProductComponent, canActivate: [AuthGuard] }
+  { path: 'products', component: ProductComponent, canActivate: [AuthGuard] },
+  { path: 'error', component: ErrorComponent }
 ];
 
 @NgModule({
