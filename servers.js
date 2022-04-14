ed1b10285b924456d0da1f3d975152ba341c6c1a
Adrian Ravai <ravaiadrian@gmail.com>
2022-04-14 12:52:31 +0200

    More deployment stuff

--- servers.js
+++ servers.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/jigokucade'));
+app.use(express.static(__dirname + '/dist/prf-shop'));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/jigokucade/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/prf-shop/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
-
